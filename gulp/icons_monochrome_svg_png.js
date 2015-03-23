3272d274606a443b0c764a2781c64c3b0c071e3e
kristjanjansen <kristjan.jansen@gmail.com>
2015-03-23 13:12:43 +0000

    latest

--- gulp/icons_monochrome_svg_png.js
+++ gulp/icons_monochrome_svg_png.js
@@ -6,9 +6,53 @@ var del = require('del');
 var filter    = require('gulp-filter');
 var svg2png   = require('gulp-svg2png');
 
+var lazypipe = require('lazypipe');
+var clone = require('gulp-clone');
+var cheerio = require('gulp-cheerio');
+var rename = require('gulp-rename');
+
+
+/*
+function isNotColorized (file) {
+  var parts = path.basename(file.relative, '.svg').split('--')
+  return (parts.length > 1) ? false : true;
+}
+*/
+
+function colorize (colorConf) {
+  var sink;
+  return (lazypipe()
+      .pipe(function () {
+          sink = clone.sink();
+          return sink;
+      })
+      .pipe(cheerio, {
+        parserOptions: { xmlMode: true },
+        run: function ($, file, done) {
+          $('path').attr('fill', colorConf.color)
+          done();
+        }
+      })
+      .pipe(rename, { suffix: '---' + colorConf.name })
+      .pipe(function () {
+          return sink.tap();
+      })
+  )();
+}
+
 gulp.task('icons_monochrome_svg_png', function () {
 
   var config = {
+    afterTransform: function (data) {
+      data.svg.map(function (item) {
+        var el = item.name.split('---').reverse()
+        if (el.length > 1) {
+          item.name = el[0] + '.icon-mono-' + el[1]
+        }
+        return item
+      })
+      return data
+    },
     common: 'icon-mono',
     baseSize: 32,
     padding: 10,
@@ -23,6 +67,7 @@ gulp.task('icons_monochrome_svg_png', function () {
   };
 
   gulp.src('assets/svg/monochrome/*.svg')
+    .pipe(colorize({name: 'inverted', color: '#fff'}))
     .pipe(svgsprites(config))
     .pipe(gulp.dest('public'))
     .pipe(filter('**/*.svg'))
--- gulpfile.js
+++ gulpfile.js
@@ -4,5 +4,6 @@ var requireDir = require('require-dir');
 requireDir('./gulp', { recurse: true });
 
 gulp.task('production', ['icons_color', 'icons_monochrome_font', 'icons_monochrome_svg_png', 'js']);
+gulp.task('mono', ['icons_monochrome_font', 'icons_monochrome_svg_png', 'js']);
 
-gulp.task('default', ['icons_bitmap']);
+gulp.task('default', ['production']);
